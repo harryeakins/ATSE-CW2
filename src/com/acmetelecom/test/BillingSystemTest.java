566b77ac1931e203a845025d2c78b5cca23fe40d
Allan Wojciechowski <allanwoj@gmail.com>
2011-11-30 14:37:32 +0000

    Implementation of the TestTimeGetter and off peak test case.

--- src/com/acmetelecom/test/BillingSystemTest.java
+++ src/com/acmetelecom/test/BillingSystemTest.java
@@ -1,5 +1,22 @@
 package com.acmetelecom.test;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import org.junit.Test;
+
+import com.acmetelecom.BillingSystem;
+import com.acmetelecom.SystemTimeGetter;
 
 public class BillingSystemTest {
-
+	@Test
+	public void offPeak() throws Exception {
+		ArrayList<Date> times = new ArrayList<Date>();
+		times.add(new Date(2011, 11, 29, 14, 0, 0));
+		times.add(new Date(2011, 11, 29, 14, 20, 0));
+		System.out.println("Running...");
+		BillingSystem billingSystem = new BillingSystem(new TestTimeGetter(times));
+		billingSystem.callInitiated("447722113434", "447766814143");
+		billingSystem.callCompleted("447722113434", "447766814143");
+		billingSystem.createCustomerBills();
+	}
 }
--- src/com/acmetelecom/test/TestTimeGetter.java
+++ src/com/acmetelecom/test/TestTimeGetter.java
@@ -1,13 +1,20 @@
 package com.acmetelecom.test;
 
+import java.util.Date;
+import java.util.List;
+
 import com.acmetelecom.TimeGetter;
 
 public class TestTimeGetter implements TimeGetter {
 
+	private List<Date> times;
+	
+	public TestTimeGetter(List<Date> times) {
+		this.times = times;
+	}
 	@Override
 	public long getCurrentTime() {
-		// TODO Auto-generated method stub
-		return 0;
+		return times.remove(0).getTime();
 	}
 
 }
