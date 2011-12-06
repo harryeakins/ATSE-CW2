611dd964948f8bb465517c19004adaaacbd8ceaa
Hui Qi Tay <tayhuiqithq@gmail.com>
2011-12-06 00:12:02 +0000

    Added a lame mock test on billingSystem.createCustomerBills If it can't find stuff:- 1) Add hamcrest, jmock, junit from AudioGuide tutorial to buildpath 2) Remove current JUnit library if necessary, if hamcrest signature conflicts

---
+++ src/com/acmetelecom/test/MockBillingSystemTest.java
@@ -0,0 +1,48 @@
+package com.acmetelecom.test;
+import java.util.List;
+
+import org.jmock.Expectations;
+import org.jmock.integration.junit4.JMock;
+import org.jmock.integration.junit4.JUnit4Mockery;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.acmetelecom.BillGenerator;
+import com.acmetelecom.BillingSystem;
+import com.acmetelecom.TimeGetter;
+import com.acmetelecom.customer.*;
+
+@RunWith(JMock.class)
+public class MockBillingSystemTest {
+
+	@Rule public final JUnit4Mockery context = new JUnit4Mockery();
+	
+    BillingSystem billingSystem;
+    final TimeGetter timeGetter = context.mock(TimeGetter.class);
+    final TariffLibrary tariffLibrary = context.mock(TariffLibrary.class);
+    final BillGenerator billGenerator = context.mock(BillGenerator.class);
+    final CustomerDatabase customerDatabase = context.mock(CustomerDatabase.class);
+    
+    
+    @SuppressWarnings("unchecked")
+	@Test
+	public void offPeak() throws Exception {
+        final List<Customer> customerList = context.mock(List.class);
+        
+        billingSystem = new BillingSystem(  timeGetter, 
+        									billGenerator,
+        									tariffLibrary,
+                                            customerDatabase
+        									);
+        
+		context.checking(new Expectations(){{
+			allowing(customerDatabase).getCustomers();
+			allowing(customerList).iterator();
+			will(returnValue(customerList));
+		}});
+		
+		billingSystem.createCustomerBills();
+	}
+}
+
